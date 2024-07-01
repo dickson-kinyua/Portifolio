6a66767e8395d3f7399ebd51e72bc98f817a61b9
Deecky <dickykinyua@gmail.com>
2024-07-01 09:03:24 +0300

    layout

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const modal = document.querySelector(".skills_modal");
-const introButtton = document.querySelector(".intro_button");
+const viewSkills = document.querySelector(".view_skills");
 const skillSection = document.querySelector(".skills_wrapper");
 const mainHeader = document.querySelector(".main_header");
-const introButton = document.querySelector(".intro_button");
-const introButton1 = document.querySelector(".intro_button1");
+// const introButton = document.querySelector(".intro_button");
+const download_cv = document.querySelector(".download_cv-btn");
 const closeButton = document.querySelector(".close_button");
 const overview = document.querySelector(".overview");
 const overviewWrapper = document.querySelector(".overview_wrapper");
@@ -21,16 +21,16 @@ window.addEventListener("load", () => {
   loader.classList.add("loader-hidden");
 });
 
-introButtton.addEventListener("load", () => {
-  loader.classList.add("loader-hidden");
-});
+// introButtton.addEventListener("load", () => {
+//   loader.classList.add("loader-hidden");
+// });
 
 // introButtton.addEventListener("click", function () {
 //   modal.classList.remove("hidden");
 //   skillSection.classList.add("overlay");
 // });
 
-introButton.addEventListener("click", function () {
+viewSkills.addEventListener("click", function () {
   skillSection.classList.add("overlay");
   skillSection.classList.remove("hidden");
 });
@@ -40,7 +40,7 @@ closeButton.addEventListener("click", function () {
   skillSection.classList.add("hidden");
 });
 
-introButton1.addEventListener("click", () => {
+download_cv.addEventListener("click", () => {
   overviewWrapper.classList.add("overlay2");
   overviewWrapper.classList.remove("hidden2");
 });
